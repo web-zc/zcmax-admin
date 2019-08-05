492c36d56e998ddb20115a802aec69d59d42f616
web-zc <443662456@qq.com>
2019-08-05 20:42:40 +0800

    login-ok

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入axios
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = "http://localhost:3010/api/"
 // 引入iview
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,90 @@
 <template>
   <div class="login-content">
-   
+    <Form ref="formLogin" class="login-form" :model="formLogin" :rules="ruleLogin">
+      <h1 class="login-title">跳蚤后台管理</h1>
+      <FormItem prop="name">
+        <Input type="text" v-model="formLogin.name">
+          <Icon type="ios-person-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem prop="password">
+        <Input type="password" v-model="formLogin.password">
+          <Icon type="ios-lock-outline" slot="prepend"></Icon>
+        </Input>
+      </FormItem>
+      <FormItem>
+        <Button type="primary" @click="handleSubmit('formLogin')" long>登陆</Button>
+        <!-- <Button @click="handleReset('formLogin')" style="margin-left: 8px">Reset</Button> -->
+      </FormItem>
+    </Form>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      formLogin: {
+        name:'',
+        password:''
+      },
+      ruleLogin: {
+        name: [
+          {
+            required: true,
+            message: "用户名不能为空",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+     handleSubmit(name) {
+      this.$refs[name].validate( async valid => {
+        if (valid) {
+         
+          const res = this.$axios.post('/users/login', this.formLogin)
+          this.$Message.success("登陆成功");
+        } else {
+          this.$Message.error("登陆失败");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang='scss' scoped>
- .login-content {
-   height: 100%;
-   background: url(../../public/images/bg-1.jpg) no-repeat center;
-   background-size: cover;
- }
+.login-content {
+  height: 100%;
+  background: url(../../public/images/bg-2.jpg) no-repeat center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  .login-form {
+    border-radius: 6px;
+    width: 300px;
+    padding: 40px 30px;
+    padding-bottom: 10px;
+    background-color: rgba(0, 0, 0, 0.3);
+    text-align: center;
+    .login-title {
+      font-size: 22px;
+      color: #fff;
+      position: relative;
+      left: 0;
+      top: -20px;
+    }
+  }
+}
 </style>
