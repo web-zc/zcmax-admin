a857d4dd5b4c881e50af8c46566b0709e7e7f6b3
web-zc <443662456@qq.com>
2019-08-05 18:41:33 +0800

    login

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-content">
+   
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+ .login-content {
+   height: 100%;
+   background: url(../../public/images/bg-1.jpg) no-repeat center;
+   background-size: cover;
+ }
+</style>
